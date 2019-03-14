b5f7fe80c80adc1636d8bb944383f3303b45a7b0
Yurii LT <yurii.horodnyi@gmail.com>
2019-03-14 19:25:53 +0200

    Zapas pri prokrutke

--- js/custom.js
+++ js/custom.js
@@ -21,7 +21,7 @@
       }
       var $anchor = $(this);
       $('html, body').stop().animate({
-        scrollTop: $($anchor.attr('href')).offset().top
+        scrollTop: $($anchor.attr('href')).offset().top-($('ul').height()+ 100)
       }, 1500, 'easeInOutExpo');
       event.preventDefault();
     });
